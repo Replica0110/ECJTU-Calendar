83eab26abe739e341964db2e8b21ce1d4c0b0ad3
Agiens02 <125732107+Agiens02@users.noreply.github.com>
2024-09-09 08:58:58 +0800

    与使用无关的编译细节改动

--- app/src/main/java/com/lonx/ecjtu/hjcalendar/fragments/SettingsFragment.kt
+++ app/src/main/java/com/lonx/ecjtu/hjcalendar/fragments/SettingsFragment.kt
@@ -4,76 +4,54 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
+import com.lonx.ecjtu.hjcalendar.BuildConfig
 import com.google.android.material.textfield.TextInputEditText
 import com.lonx.ecjtu.hjcalendar.R
 import com.lonx.ecjtu.hjcalendar.utils.ToastUtil
 
-
 class SettingsFragment : PreferenceFragmentCompat() {
 
-    private val githubIntent by lazy { Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Agiens02")) }
-    private val sourceCodeIntent by lazy { Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Agiens02/ECJTU-Calendar")) }
+    private val developerIntent by lazy { Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.developer_url)))}
+    private val sourceCodeIntent by lazy { Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.source_code_url)))}
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences_settings, rootKey)
+        setVersionInfo()
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        val context = requireContext()
         when (preference.key) {
-            getString(R.string.developer) -> openURL(githubIntent)
+            getString(R.string.developer) -> openURL(developerIntent)
             getString(R.string.source_code_key) -> openURL(sourceCodeIntent)
-            getString(R.string.tutorial_weixinid_key) -> showAlertDialog(context, preference.title, R.string.tutorial_weixinid_message)
-            getString(R.string.tutorial_date_key) -> showAlertDialog(context, preference.title, R.string.tutorial_date_message)
-            getString(R.string.weixin_id_key) -> showInputDialog(preference)
-            getString(R.string.no_course_key) -> showInputDialog(preference)
+            getString(R.string.tutorial_weixinid_key) -> showAlertDialog(preference.title, R.string.tutorial_weixinid_message)
+            getString(R.string.tutorial_date_key) -> showAlertDialog(preference.title, R.string.tutorial_date_message)
+            getString(R.string.weixin_id_key), getString(R.string.no_course_key) -> showInputDialog(preference)
         }
         return super.onPreferenceTreeClick(preference)
     }
 
-    private fun showAlertDialog(context: Context, titleResId: CharSequence?, messageResId: Int) {
-        MaterialAlertDialogBuilder(context)
+    private fun showAlertDialog(titleResId: CharSequence?, messageResId: Int) {
+        MaterialAlertDialogBuilder(requireContext())
             .setTitle(titleResId)
             .setMessage(getString(messageResId))
             .show()
     }
 
-
     private fun showInputDialog(preference: Preference) {
-        Log.e("SettingsFragment", "点击了${preference.title}")
         val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_edittext, null)
         val textInput = dialogView.findViewById<TextInputEditText>(R.id.text_input)
         val sharedPreferences = requireActivity().getSharedPreferences("SettingsPrefs", Context.MODE_PRIVATE)
-        if (preference.key == getString(R.string.weixin_id_key)){
-            val savedValue = sharedPreferences.getString("weixin_id", "")  // 使用固定键
-            textInput.setText(savedValue)
-
-            MaterialAlertDialogBuilder(requireContext())
-                .setTitle(preference.title)
-                .setView(dialogView)
-                .setPositiveButton("保存") { dialog, _ ->
-                    val inputText = textInput.text.toString()
-
-                    with(sharedPreferences.edit()) {
-                        putString("weixin_id", inputText)
-                        apply()
-                    }
-                    ToastUtil.showToast(requireContext(), "weiXinID已保存，在日历页面下拉刷新课表")
-                    dialog.dismiss()
-                }
-                .setNegativeButton("取消") { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .show()
-    } else if (preference.key == getString(R.string.no_course_key))
-    {
-        val savedValue = sharedPreferences.getString("no_course_text", "")
+        val key = when (preference.key) {
+            getString(R.string.weixin_id_key) -> "weixin_id"
+            getString(R.string.no_course_key) -> "no_course_text"
+            else -> ""
+        }
+        val savedValue = sharedPreferences.getString(key, "")
         textInput.setText(savedValue)
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(preference.title)
@@ -81,19 +59,16 @@ class SettingsFragment : PreferenceFragmentCompat() {
             .setPositiveButton("保存") { dialog, _ ->
                 val inputText = textInput.text.toString()
                 with(sharedPreferences.edit()) {
-                    putString("no_course_text", inputText)
+                    putString(key, inputText)
                     apply()
                 }
-                ToastUtil.showToast(requireContext(), "自定义文本已保存")
+                val message = if (key == "weixin_id") "weiXinID已保存，在日历页面下拉刷新课表" else "自定义文本已保存"
+                ToastUtil.showToast(requireContext(), message)
                 dialog.dismiss()
             }
-            .setNegativeButton("取消") { dialog, _ ->
-                dialog.dismiss()
-            }
+            .setNegativeButton("取消") { dialog, _ -> dialog.dismiss() }
             .show()
     }
-    }
-
 
     private fun openURL(intent: Intent) {
         try {
@@ -102,5 +77,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
             e.printStackTrace()
             Snackbar.make(requireView(), getString(R.string.snackbar_intent_failed), Snackbar.LENGTH_SHORT).show()
         }
+    }
+    private fun setVersionInfo() {
+        // 获取应用版本号、版本名和编译时间
+        val versionName = BuildConfig.VERSION_NAME
+        val versionCode = BuildConfig.VERSION_CODE
+        val buildTime = BuildConfig.BUILD_TIME
+
+        // 查找用于显示版本信息的Preference
+        val versionPreference: Preference? = findPreference(getString(R.string.app_version_key))
+
+        // 设置版本信息到summary中
+        versionPreference?.summary = "版本: $versionName ($versionCode)\n最后编译时间: $buildTime"
     }
 }

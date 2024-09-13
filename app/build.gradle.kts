7724a66e1686424bdf5501ef016300867788e72d
Agiens02 <125732107+Agiens02@users.noreply.github.com>
2024-09-13 14:40:57 +0800

    更新小组件，存在问题 - bug: 无法自动刷新或使用按钮刷新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,7 +122,6 @@ dependencies {
     implementation(libs.androidx.preference.ktx)
     implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.androidx.work.runtime.ktx)
-    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

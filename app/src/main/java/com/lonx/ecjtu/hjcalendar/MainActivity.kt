fd0f33c65eeffe6a3092c159a371809471c0b2ea
可燃乌龙茶 <125732107+Agiens02@users.noreply.github.com>
2025-03-08 17:31:55 +0800

    feat(app): 添加Deep Link功能并更新相关配置
    - 添加对“在浏览器中打开”选项的支持
    - 调整了项目编译配置，将 Java 版本升级到 21

--- app/src/main/java/com/lonx/ecjtu/hjcalendar/MainActivity.kt
+++ app/src/main/java/com/lonx/ecjtu/hjcalendar/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.lonx.ecjtu.hjcalendar
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.preference.PreferenceManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.lonx.ecjtu.hjcalendar.fragment.CalendarFragment
 import com.lonx.ecjtu.hjcalendar.fragment.SettingsFragment
+import com.lonx.ecjtu.hjcalendar.utils.ToastUtil
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
     private lateinit var bottomNav: BottomNavigationView
+    private val prefs by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        parseIntent(intent)
         setContentView(R.layout.activity_main)
 
         // 初始化ViewPager
@@ -62,6 +68,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        parseIntent(intent)
+    }
+    private fun parseIntent(intent: Intent?) {
+        if (intent?.action == Intent.ACTION_VIEW) {
+            val uri = intent.data
+            uri?.let {
+                val weixinID = it.getQueryParameter("weiXinID")
+                weixinID?.let { id ->
+                    prefs.edit().putString("weixin_id", id).apply()
+                }
+            }
+        }
+    }
     private fun updateActionBarTitle(title: String) {
         supportActionBar?.title = title
     }

40c09543fb9fb4aa11645c37be0a638be49d4595
Replica0110 <kai421539@gmail.com>
2025-08-19 17:09:57 +0800

    feat(security): 为 ecjtu.edu.cn 添加证书固定
    - 在项目中添加 ecjtu.crt 证书文件
    - 更新 network_security_config.xml，为 ecjtu.edu.cn 域名配置证书固定
    - 移除 NetworkModule.kt 中不安全的 SSLContext 和 TrustManager 实现

--- app/src/main/java/com/lonx/ecjtu/hjcalendar/utils/NetworkModule.kt
+++ app/src/main/java/com/lonx/ecjtu/hjcalendar/utils/NetworkModule.kt
@@ -1,33 +1,12 @@
 package com.lonx.ecjtu.hjcalendar.utils
 
 import okhttp3.OkHttpClient
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 object NetworkModule {
 
     val okHttpClient: OkHttpClient by lazy {
-        // 创建一个信任所有证书的 TrustManager
-        val trustAllCerts = arrayOf<TrustManager>(
-            object : X509TrustManager {
-                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
-                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
-                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-            }
-        )
-
-        // 创建 SSLContext
-        val sslContext: SSLContext = SSLContext.getInstance("SSL").apply {
-            init(null, trustAllCerts, SecureRandom())
-        }
-
         // 构建 OkHttpClient
         OkHttpClient.Builder()
-            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
-            .hostnameVerifier { _, _ -> true } // 也信任所有主机名
             .connectTimeout(10, java.util.concurrent.TimeUnit.SECONDS)
             .readTimeout(10, java.util.concurrent.TimeUnit.SECONDS)
             .writeTimeout(10, java.util.concurrent.TimeUnit.SECONDS)

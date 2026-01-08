824a8ae83b7e4409845d99d7cd0ceeee5a400941
Replica0110 <125732107+Replica0110@users.noreply.github.com>
2026-01-08 14:53:35 +0800

    build(app): 启用资源压缩优化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ android {
 
     buildTypes {
         release {
+            isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
